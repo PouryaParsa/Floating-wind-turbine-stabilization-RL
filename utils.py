d519c0a3827e113177d7bb1e27689911f1943281
Billy <34207387+PouryaParsa@users.noreply.github.com>
2023-12-31 22:35:29 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,40 +2,40 @@ import control
 import slycot
 import numpy as np
 from numpy.linalg import matrix_rank
-from scipy.io import savemat
-from scipy.io import loadmat
-import gym_turbine.utils.state_space as ss
-
+from scipy.io import savemat, loadmat
 from pandas import DataFrame
+import gym_turbine.utils.state_space as ss
 
 def simulate_episode(env, agent, max_time, lqr=False):
     global input_labels, state_labels
     state_labels = [r"x_sg", r"x_sw", r"x_hv", r"theta_r", r"theta_p", r"x_tf", r"x_ts", r"x_1", r"x_2", r"x_3", r"x_4"]
     state_dot_labels = [r"x_sg_dot", r"x_sw_dot", r"x_hv_dot", r"theta_r_dot", r"theta_p_dot", r"x_tf_dot", r"x_ts_dot", r"x_1_dot", r"x_2_dot", r"x_3_dot", r"x_4_dot"]
-    input_labels = [r"Fa_1", r"Fa_2", r"Fa_3",  r"Fa_4"]
-    reward_labels = r"reward"
-    labels = np.hstack(["time", state_labels, state_dot_labels, input_labels, reward_labels])
+    input_labels = [r"Fa_1", r"Fa_2", r"Fa_3", r"Fa_4"]
+    reward_label = r"reward"
+    labels = np.hstack(["time", state_labels, state_dot_labels, input_labels, reward_label])
 
     if lqr:
-        K = loadmat('gym_turbine\\utils\\Ksys.mat')["Ksys_lqr"]     # Use K calculated in code from wiley paper
-        # K = loadmat('gym_turbine\\utils\\LQR_params.mat')["K"]      # Use K calculated in matlab using own system matrices
+        K = loadmat('gym_turbine\\utils\\Ksys.mat')['Ksys_lqr']  # Use K calculated in code from wiley paper
+        # K = loadmat('gym_turbine\\utils\\LQR_params.mat')['K']   # Use K calculated in Matlab using own system matrices
 
     done = False
     env.reset()
-    while not done and env.t_step < max_time/env.step_size:
+    while not done and env.t_step < max_time / env.step_size:
         if lqr:
-            action = (-K.dot(env.turbine.state))/ss.max_input
+            action = (-K.dot(env.turbine.state)) / ss.max_input
         else:
             action, _states = agent.predict(env.observation, deterministic=True)
         _, _, done, _ = env.step(action)
 
     time = np.array(env.episode_history['time']).reshape((env.t_step, 1))
     last_reward = np.array(env.episode_history['last_reward']).reshape((env.t_step, 1))
-    sim_data = np.hstack([  time,
-                            env.episode_history['states'],
-                            env.episode_history['states_dot'],
-                            env.episode_history['input'],
-                            last_reward,
-                        ])
+    sim_data = np.hstack([
+        time,
+        env.episode_history['states'],
+        env.episode_history['states_dot'],
+        env.episode_history['input'],
+        last_reward,
+    ])
     df = DataFrame(sim_data, columns=labels)
     return df
+
